aec043b1290c801623633de1902c95cb22702ff2
raymondcheng717 <raymondcheng717@gmail.com>
2023-10-21 20:31:33 -0500

    Student Profile Added

--- Pages/2_Student_Profile.py
+++ Pages/2_Student_Profile.py
@@ -9,29 +9,27 @@ def student_profile_page():
 
     new_student = st.session_state.new_student
 
-    if st.button("Edit Profile"):
-        new_student = edit_profile(new_student)  # Get the updated student object
-    
-    display_student_profile(new_student)
+    display_student_profile(new_student)  # Display the profile at the top
 
-def edit_profile(student):
-    st.title("Edit Profile")
-    
-    first_name = st.text_input("First Name", student.first_name)
-    last_name = st.text_input("Last Name", student.last_name)
-    eid = st.text_input("EID", student.eid)
-    major = st.text_input("Major", student.major)
+    # Add a visual separation and style the "Edit Profile" section
+    st.markdown("<hr style='margin: 10px;'>", unsafe_allow_html=True)
+    st.markdown("<h2 style='font-weight: bold;'>Edit Profile</h2>", unsafe_allow_html=True)
+
+    first_name = st.text_input("First Name", new_student.first_name)
+    last_name = st.text_input("Last Name", new_student.last_name)
+    eid = st.text_input("EID", new_student.eid)
+    major = st.text_input("Major", new_student.major)
     classification = st.selectbox("Classification", ["Freshman", "Sophomore", "Junior", "Senior"], index=2)  # Default to "Junior"
-    minor = st.text_input("Minor", student.minor)
+    minor = st.text_input("Minor", new_student.minor)
 
     if st.button("Save Changes"):
-        # Create a new student with the modified information
-        updated_student = Student(first_name, last_name, eid, major, classification, minor)
-        st.session_state.new_student = updated_student  # Update the session state with the new student object
-        st.experimental_rerun()
-        return updated_student  # Return the updated student object
-
-    return student  # Return the original student object if changes are not saved
+        # Update the student object with the modified information
+        new_student.first_name = first_name
+        new_student.last_name = last_name
+        new_student.eid = eid
+        new_student.major = major
+        new_student.classification = classification
+        new_student.minor = minor
 
 def display_student_profile(student):
     st.write(f"Name: {student.first_name} {student.last_name}")
